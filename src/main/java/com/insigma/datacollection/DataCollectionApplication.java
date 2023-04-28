8fbbcf2363e0ccef233b2583787d66616ee0ba34
songyh <12345678>
2023-04-28 11:36:51 +0800

    feat(全部)初始化项目

---
+++ src/main/java/com/insigma/datacollection/DataCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.insigma.datacollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollectionApplication.class, args);
+    }
+
+}
